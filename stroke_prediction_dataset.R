d47ca3cb84310f3c82a14768ea7381bed78cb6cd
Raiyan Al Sultan <rraiyan77@gmail.com>
2023-11-30 20:10:54 +0600

    Recovering bmi column outliers with mode value

--- stroke_prediction_dataset.R
+++ stroke_prediction_dataset.R
@@ -112,3 +112,11 @@ cat("Potential rows of missing value in BMI: ", bmi_missing_value_rows, "\n")
 # Recovering missing values with mode and median in BMI column
 stroke_prediction_dataset$bmi[is.na(stroke_prediction_dataset$bmi)] <- mode_bmi
 stroke_prediction_dataset$bmi[is.na(stroke_prediction_dataset$bmi)] <- median_bmi
+
+
+# Recovering outliers with mode value in bmi column
+for (i in 1:length(stroke_prediction_dataset$bmi)){
+  if (stroke_prediction_dataset$bmi[i] < 0 | stroke_prediction_dataset$bmi[i] > 40){
+    stroke_prediction_dataset$bmi[i] <- mode_bmi
+  }
+}
